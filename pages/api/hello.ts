27e65055397f5a83277c97ff8b8ae98b632fb9dc
Tài <hotai1806@gmail.com>
2023-07-26 17:35:56 +0700

    add source vocabulary

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {readFileSync} from 'fs'
 
-type Data = {
-  name: string
+export type Data = {
+  name: string,
+  num: number
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
+  
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  let randomnumber=Math.random();
+  const result = await readFileSync("../../Remember-Tradional-Hanzi-1.txt","utf-8")
+  res.setHeader(
+
+    'Cache-Control','public, max-age=9999999999, must-revalidate'
+  ).status(200).json({ name: 'John Doe',num:randomnumber })
+  return new Response(
+    JSON.stringify({
+      name: 'Jim Halpert'+ randomnumber,
+    }),
+    {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+        'cache-control': 'public, s-maxage=1200, stale-while-revalidate=600',
+      },
+    }
+  )
 }
